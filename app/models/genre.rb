d7ec3ddd57dfe6b3436cc135eca32cc47c1c5419
shincho0922 <79453715+shincho0922@users.noreply.github.com>
2021-04-08 18:36:36 +0900

    細かいビューの編集

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,6 @@
 class Genre < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
+    { id: 1, name: 'ジャンル' },
     { id: 2, name: '洋食' },
     { id: 3, name: '中華' },
     { id: 4, name: '和食' },
