7f542fc8e0684a57eb2499df8bf969f50d25a794
shincho0922 <79453715+shincho0922@users.noreply.github.com>
2021-04-15 19:33:43 +0900

    いいね機能変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,12 @@ class LikesController < ApplicationController
   before_action :set_tweet
 
   def create
-    @like = Like.create(user_id: current_user.id, tweet_id: @tweet.id)
+    @like = Like.create(user_id: current_user.id, tweet_id: params[:tweet_id])
   end
 
   def destroy
-    @like = Like.find_by(user_id: current_user.id, tweet_id: @tweet.id)
-    @like.destroy
+    like = Like.find_by(user_id: current_user.id, tweet_id: params[:tweet_id])
+    like.destroy
   end
 
   private
