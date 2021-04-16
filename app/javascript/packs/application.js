3bcffda05b969a6fb4af16ea2bf7f2904fa34ea1
shincho0922 <79453715+shincho0922@users.noreply.github.com>
2021-04-16 11:51:46 +0900

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,12 +4,9 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-// require("turbolinks").start()
+require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
-require("@rails/ujs").start()
-require("turbolinks").start()
 require('jquery')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
